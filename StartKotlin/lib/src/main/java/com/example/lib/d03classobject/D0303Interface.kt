5cce47b5f329208a4b503def6f1e454ed57302cb
Cai Huijian <hjcai@telenav.cn>
2021-08-25 10:07:58 +0800

    第二章 类与对象 接口 函数式接口完成

--- StartKotlin/lib/src/main/java/com/example/lib/d03classobject/D0303Interface.kt
+++ StartKotlin/lib/src/main/java/com/example/lib/d03classobject/D0303Interface.kt
@@ -1,12 +1,12 @@
 package com.example.lib.d03classobject
 
 fun main() {
+    // 测试接口的覆盖
     val myChild = D0303Interface.Child()
     println(myChild.prop)
     println(myChild.propertyWithImplementation)
 }
 
-// 4 接口继承 TODO
 class D0303Interface {
     // 2 如何实现接口
     class Child : MyInterface {
@@ -45,5 +45,70 @@ interface MyInterface {
         // 可选的⽅法体
         // 与Java不同的是 Kotlin的接口可以有具体的实现
         println("foo")
+    }
+}
+
+// 4 接口继承接口
+// 一个接⼝可以从其他接⼝派⽣，从⽽既提供基类型成员的实现也声明新的函数与属性。很⾃然地，实现这样接⼝
+// 的类只需定义所缺少的实现
+interface Named {
+    val name: String
+}
+
+interface Person : Named {
+    //接口Person继承接口Named
+    val firstName: String
+    val lastName: String
+
+    // 实现继承自Named的属性的get方法 返回firstName+lastName
+    override val name: String get() = "$firstName $lastName"
+}
+
+// 数据类继承自Person 因为Person内部已经override了name 所以Employee不一定需要重写name属性
+// 关于data class 后面再讲
+data class Employee(
+        // 不必实现“name”
+        override val firstName: String,
+        override val lastName: String,
+) : Person
+
+
+// 5 解决接口上的覆盖冲突
+// 实现多个接⼝时，可能会遇到同⼀⽅法继承多个实现的问题
+interface A {
+    fun foo() {
+        print("fooA")
+    }
+
+    fun bar()// 默认是抽象方法
+}
+
+interface B {
+    fun foo() {
+        print("fooB")
+    }
+
+    fun bar() {
+        print("barB")
+    }
+}
+
+class C : A {
+    // 抽象方法必须重写
+    override fun bar() {
+        print("barC")
+    }
+}
+
+class D : A, B {
+    // 这里的继承规则在类与对象-类与继承第16点 中也讲过
+    // 不管是继承接口 还是继承类 调用父类/父接口的方式不变
+    override fun foo() {
+        super<A>.foo()
+        super<B>.foo()
+    }
+
+    override fun bar() {
+        super<B>.bar()
     }
 }
--- StartKotlin/lib/src/main/java/com/example/lib/d03classobject/D0304SamInterface.kt
+++ StartKotlin/lib/src/main/java/com/example/lib/d03classobject/D0304SamInterface.kt
@@ -1,3 +1,42 @@
 package com.example.lib.d03classobject
 
-class D0304SamInterface 
+fun main() {
+    // 2 函数式接口的应用2
+    // 测试使用SAM函数
+    println("res = ${res1.add(2, 9)}")
+}
+
+class D0304SamInterface {}
+
+// 1 什么是函数式（SAM）接⼝
+// 只有⼀个抽象⽅法的接⼝称为函数式接⼝或 SAM（Single Abstract Method 单⼀抽象⽅法）接⼝。函数式接⼝可以有多个⾮抽象成员，但
+// 只能有⼀个抽象成员。
+// 可以⽤ fun 修饰符在 Kotlin 中声明⼀个函数式接⼝。
+// 函数式接口 VS 普通接口
+// 在interface关键字前+fun 并且接口只有一个方法
+// 那么这个接口就是 函数式（SAM）接⼝
+fun interface KRunnable {
+    fun invoke()
+    // 如果添加这一句话会报错
+    // Fun interfaces must have exactly one abstract method
+    // 函数式接口只能有一个抽象方法
+    // fun invoke2()
+}
+
+// 2 函数式接口的应用1:SAM 转换
+// SAM 转换 即使⽤ lambda 表达式替代创建实现函数式接⼝的类
+// 例如有一个函数式接口
+fun interface IntPredicate {
+    fun add(i: Int, j: Int): Int
+}
+
+// 不用SAM转换 即直接继承接口
+val res = object : IntPredicate {
+    override fun add(i: Int, j: Int): Int {
+        return i + j
+    }
+}
+
+// 使用SAM转换
+val res1 = IntPredicate { i, j -> i + j }
+// 个人猜测 函数式接口只有一个抽象接口 那么可以 用接口名直接代表抽象方法 方法体内部就是lambda表达式的抽象接口的实现
